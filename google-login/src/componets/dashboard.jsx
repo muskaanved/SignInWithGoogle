e641410bde8ac927c90a3983c20b4dc3e1649263
muskaanved <muskanved0@gmail.com>
2022-04-05 11:31:13 +0530

    5 th take pull

--- google-login/src/componets/dashboard.jsx
+++ google-login/src/componets/dashboard.jsx
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import Login from './login';
 import Sidebar from './sidebar';
 class Dashboard extends Component {
-    
-    render() { 
+
+    render() {
         return (
             <React.Fragment>
                 <div className='sidebar'>
-                     <Sidebar/> 
+                    <Sidebar />
                 </div>
                 <div className='content-before-login content'>
-                     <div className='google-login'>
-                         <h3>AFFIDAVITS & RFI</h3>
-                         <h2> Looks like you're not yet connected with your Google Account </h2>
-                         <span>Lets make your trip fun and simple</span>
-                     </div>
-                    <Login/>
+                    <div className='google-login'>
+                        <h3>AFFIDAVITS & RFI</h3>
+                        <h2> Looks like you're not yet connected with your Google Account </h2>
+                        <span>Lets make your trip fun and simple</span>
+                    </div>
+                    <Login />
                 </div>
             </React.Fragment>
         );
     }
 }
- 
+
 export default Dashboard;
--- google-login/src/componets/login.jsx
+++ google-login/src/componets/login.jsx
@@ -1,71 +1,70 @@
-import React, { Component } from 'react';
-import Sidebar from './sidebar';
+import React from 'react';
 import GoogleLogin, { GoogleLogout } from 'react-google-login';
 import { useState } from 'react';
 
 function Login() {
 
-    const [loginData, setLoginData] = useState(
-        localStorage.getItem('loginData')?
-        JSON.parse(localStorage.getItem('loginData'))
-        :null
-      );
-    
-      const handleFailure = (result) => {
-        console.log(result);
-      }
-    
-      const handleLogin = async(googleData) => {
-        const res = await fetch('/api/google-login',{
-          method: 'POST',
-          body: JSON.stringify({
-            token:googleData.tokenId,
-          }),
-          headers:{
-            'Content-Type':'application/json',
-          },
-        });
-    
-        const data = await res.json();
-        setLoginData(data);
-        localStorage.setItem('loginData',JSON.stringify(data));
-    
-      }
-    
-      const handleLogout = () => {
-        localStorage.removeItem('loginData');
-        setLoginData(null);
-      };
-   
-    
-        return (
-            <React.Fragment>
-        
-                   
-            <div>
-                        {loginData ? (<div>
-                        <h3>you logged in as {loginData.email}</h3>
-                        
-                        <GoogleLogout
-                                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                                buttonText="Logout"
-                                onLogoutSuccess={handleLogout}>
-                                </GoogleLogout>
-                        </div>): (
-                            <GoogleLogin className='google-login-button'
-                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                            buttonText="Sign in with Google"
-                            onSuccess={handleLogin}
-                            onFailure={handleFailure}
-                            cookiePolicy={'single_host_origin'}>
-                            </GoogleLogin>
-                            )}
-            </div>
-                   
-                
-            </React.Fragment>
-        );
-    }
+	const [loginData, setLoginData] = useState(
+		localStorage.getItem('loginData') ?
+			JSON.parse(localStorage.getItem('loginData'))
+			: null
+	);
 
- 
-export default Login ;
+	const handleFailure = (result) => {
+		console.log(result);
+	}
+
+	const handleLogin = async (googleData) => {
+		const res = await fetch('/api/google-login', {
+			method: 'POST',
+			body: JSON.stringify({
+				token: googleData.tokenId,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		const data = await res.json();
+		setLoginData(data);
+		localStorage.setItem('loginData', JSON.stringify(data));
+
+	}
+
+	const handleLogout = () => {
+		localStorage.removeItem('loginData');
+		setLoginData(null);
+	};
+
+
+	return (
+		<React.Fragment>
+
+
+			<div>
+				{loginData ? (<div>
+					<h3>you logged in as {loginData.email}</h3>
+
+					<GoogleLogout
+						clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+						buttonText="Logout"
+						onLogoutSuccess={handleLogout}>
+					</GoogleLogout>
+				</div>) : (
+					<GoogleLogin className='google-login-button'
+						clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+						buttonText="Sign in with Google"
+						onSuccess={handleLogin}
+						onFailure={handleFailure}
+						cookiePolicy={'single_host_origin'}>
+					</GoogleLogin>
+				)}
+			</div>
+
+
+		</React.Fragment>
+	);
+}
+
+
+export default Login;
